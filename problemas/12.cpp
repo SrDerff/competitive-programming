d81e4396506f593c96c5646f9a34bc41811b3534
Derff <srderfflinqer@gmail.com>
2026-07-16 14:17:22 -0500

    balanced-parentheses-problem

--- problemas/12.cpp
+++ problemas/12.cpp
@@ -1,17 +1,30 @@
+/*
+Verificar parentesis
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    stack<int>symbols;
-    int x;
+    string w;
+    cin>>w;
+    stack<char>symbols;
+    char x;
     bool v=true;
-    while(cin>>x){
+    int i=-1;
+    while(++i<w.size()){
+        x=w[i];
+        if(!v) continue;
         if(x==')'){
             if(symbols.empty()){
                 v=false;
-            }else{
-                symbols.pop();
+                break;
             }
-        }
+            symbols.pop();
+            continue;
+        }        
+        symbols.push(x);
     }
+    if(!symbols.empty()) v=false;
+    cout<<(v?"yes":"no");
     return 0;
 }
