18e8aaf1157e67e95c07fa36bec91b398b15b413
Derff <srderfflinqer@gmail.com>
2026-07-24 22:36:16 -0500

    two-knights

--- problems_cses/two_knights.cpp
+++ problems_cses/two_knights.cpp
@@ -5,6 +5,11 @@ using ll = long long;
 using ull =  unsigned long long;
 
 int main(){
-    
+    ull n;
+    for(ull i=1;i<=n;i++){
+        ull size=i*i;
+        ull total_pos = size*(size-1)/2;
+        ull attack_pos = ...;
+    }
     return 0;
 }
