798f782c4ba5afa231a81cd94b8231ee207c6f95
Derff <srderfflinqer@gmail.com>
2026-08-19 01:39:22 -0500

    cses problems

--- problems_cses/creating_strings.cpp
+++ problems_cses/creating_strings.cpp
@@ -8,8 +8,8 @@
 ⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ 
 ⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
 ⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
-⠀⠀⠀⠀⠀⠀ ⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
-⠀⠀⠀⠀⠀⠀⠀⠀ ⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
 ⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ 
 ⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ 
 ⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
@@ -31,11 +31,39 @@ using namespace std;
 #define all(x) (x).begin(), (x).end()
 #define MOD 1e9+7
 
+ll fact(ll n){
+    if(n==1) return 1;
+    return n*fact(n-1);
+}
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    string s;
+    cin>>s;
+
+    ll n=s.size();
+    vin rpt('z'-'a'+1, 0);
+
+    rep(i,0,n){
+        rpt[s[i]-'a']++;
+    }
+
+    ll tt=fact(n);
+
+    rep(i,0,122-97+1){
+        if(rpt[i]>1) tt/=fact(rpt[i]);
+    }
+
+    cout<<tt<<"\n";
+
+    sort(s.begin(), s.end());
+
+    rep(i,0,tt){
+        cout<<s<<"\n";
+        next_permutation(s.begin(), s.end());
+    }
 
     return 0;
 }
