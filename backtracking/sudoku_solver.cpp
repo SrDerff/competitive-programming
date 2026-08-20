02106912807ef451156ac1b481f2bf5c6ce2074b
Derff <srderfflinqer@gmail.com>
2026-08-20 12:41:39 -0500

    backtracking: sudoku solver

---
+++ backtracking/sudoku_solver.cpp
@@ -0,0 +1,115 @@
+/*
+ ⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀  ⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀  ⢀⢴⣶⣆ 
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ 
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ 
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀ ⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀ ⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉ 
+
+        Vuelve valeria
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (ll i=(a); i<(b); i++)
+#define vin vector<int>
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define all(x) (x).begin(), (x).end()
+#define MOD 1e9+7
+
+bool isValid(vector<vin>&sudoku, int i, int j, int num, int n){
+    rep(k,0,n){
+        if(sudoku[k][j]==num && k!=i) return false;
+        if(sudoku[i][k]==num && k!=j) return false;
+    }
+
+    int startRow = (i / 3) * 3;
+    int startCol = (j / 3) * 3;
+
+    rep(r, startRow, startRow + 3) {
+        rep(c, startCol, startCol + 3) {
+            if (sudoku[r][c] == num && (r != i || c != j))
+                return false;
+        }
+    }
+
+    return true;
+}
+
+bool solve(vector<vin>&sudoku){
+    int row=-1;
+    int col=-1;
+    int n=sudoku.size();
+
+    //search for the next empty position
+    rep(i,0,n){
+        rep(j,0,n){
+            if(!sudoku[i][j]){
+                row=i;
+                col=j;
+            }
+        }
+
+        if(row!=-1) break;
+    }
+
+    //if there are no more empty positions, 
+    //then sudoku has been successfully completed
+    if(row==-1) return true;
+
+    //try options
+    rep(i,1,9+1){
+        if(isValid(sudoku, row, col, i, n)){
+            sudoku[row][col]=i;
+            if(solve(sudoku)) return true;
+
+            sudoku[row][col]=0;
+        }
+    }
+
+    return false;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n=9;
+    vector<vin> sudoku(n, vin(n, 0));
+    
+    rep(i,0,n){
+        rep(j,0,n){
+            cin>>sudoku[i][j];
+        }
+    }
+
+    if(!solve(sudoku)){
+        cout<<"No solution";
+        return 0;
+    }
+
+    cout<<"\nSolution\n\n";
+
+    rep(i,0,n){
+        rep(j,0,n){
+            cout<<sudoku[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
