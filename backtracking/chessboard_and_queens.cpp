1a85f5ac33e9cd67196daec7b67caf2ec8e05d97
Derff <srderfflinqer@gmail.com>
2026-08-21 01:57:41 -0500

    backtracking: cses

---
+++ backtracking/chessboard_and_queens.cpp
@@ -0,0 +1,83 @@
+/*
+ ⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀  ⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀  ⢀⢴⣶⣆ 
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ 
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ 
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀ ⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀ ⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉ 
+
+        Vuelve valeria
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (ll i=(a); i<(b); i++)
+#define vin vector<int>
+#define vll vector<ll>
+#define vch vector<char>
+#define vpii vector<pair<int,int>>
+#define vpll vector<pair<ll,ll>>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pll pair<ll,ll>
+#define all(x) (x).begin(), (x).end()
+#define MOD 1e9+7
+#define str string
+
+bool isValid(pii pos, vpii prev_pos){
+    for(pii x_pos : prev_pos){
+        if(x_pos.first==pos.first) return false;
+        if(x_pos.second==pos.second) return false;
+        if(abs(x_pos.first-pos.first)==abs(x_pos.second-pos.second)) return false;
+    }
+    return true;
+}
+
+void solve(ll &count, vector<vch>&grid, vpii prev_pos, int curr_row, int n){
+    rep(j,0,n){
+        if(isValid(make_pair(curr_row,j), prev_pos) && grid[curr_row][j]!='*'){
+            prev_pos.push_back(make_pair(curr_row, j));
+            if(prev_pos.size()==n){
+                count++;
+                return;
+            }
+            solve(count, grid, prev_pos, curr_row+1, n);
+            prev_pos.pop_back();
+        }
+    }
+    return;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n=8;
+
+    vector<vch> grid(n, vch(n));
+    rep(i,0,n){
+        rep(j,0,n){
+            cin>>grid[i][j];
+        }
+    }
+
+    ll count=0;
+    vpii prev_pos;
+    solve(count, grid, prev_pos, 0, n);
+    cout<<count;
+
+    return 0;
+}
