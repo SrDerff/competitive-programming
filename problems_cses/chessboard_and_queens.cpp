5062df05f1370cd135c12f9d3b777b8348358381
Derff <srderfflinqer@gmail.com>
2026-08-21 01:56:56 -0500

    backtracking

--- problems_cses/chessboard_and_queens.cpp
+++ problems_cses/chessboard_and_queens.cpp
@@ -27,38 +27,57 @@ using namespace std;
 #define rep(i, a, b) for (ll i=(a); i<(b); i++)
 #define vin vector<int>
 #define vll vector<ll>
+#define vch vector<char>
+#define vpii vector<pair<int,int>>
+#define vpll vector<pair<ll,ll>>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
 #define pll pair<ll,ll>
 #define all(x) (x).begin(), (x).end()
 #define MOD 1e9+7
+#define str string
+
+bool isValid(pii pos, vpii prev_pos){
+    for(pii x_pos : prev_pos){
+        if(x_pos.first==pos.first) return false;
+        if(x_pos.second==pos.second) return false;
+        if(abs(x_pos.first-pos.first)==abs(x_pos.second-pos.second)) return false;
+    }
+    return true;
+}
+
+void solve(ll &count, vector<vch>&grid, vpii prev_pos, int curr_row, int n){
+    rep(j,0,n){
+        if(isValid(make_pair(curr_row,j), prev_pos) && grid[curr_row][j]!='*'){
+            prev_pos.push_back(make_pair(curr_row, j));
+            if(prev_pos.size()==n){
+                count++;
+                return;
+            }
+            solve(count, grid, prev_pos, curr_row+1, n);
+            prev_pos.pop_back();
+        }
+    }
+    return;
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin>>n;
-    vll nums(n);
-    ll total_sum=0;
+    int n=8;
 
-
-    rep(i,0,n){ 
-        cin>>nums[i];
-        total_sum+=nums[i];
-    }
-
-    ll min_diff=1e9;
-
-    for(int i=0;i<(1<<n);i++){
-        bitset<32> bits(i);
-        ll curr_sum=0;
+    vector<vch> grid(n, vch(n));
+    rep(i,0,n){
         rep(j,0,n){
-            if(bits[j]) curr_sum+=nums[j];
+            cin>>grid[i][j];
         }
-
-        min_diff=min(min_diff, abs(curr_sum - (total_sum-curr_sum)));
     }
 
-    cout<<min_diff;
+    ll count=0;
+    vpii prev_pos;
+    solve(count, grid, prev_pos, 0, n);
+    cout<<count;
 
     return 0;
 }
