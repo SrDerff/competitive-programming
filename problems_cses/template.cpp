5d1c2d8d344d5614f4f868ba144c026691d162c2
Derff <srderfflinqer@gmail.com>
2026-08-21 21:14:53 -0500

    template update

--- problems_cses/template.cpp
+++ problems_cses/template.cpp
@@ -27,9 +27,15 @@ using namespace std;
 #define rep(i, a, b) for (ll i=(a); i<(b); i++)
 #define vin vector<int>
 #define vll vector<ll>
+#define vch vector<char>
+#define vpii vector<pair<int,int>>
+#define vpll vector<pair<ll,ll>>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
 #define pll pair<ll,ll>
 #define all(x) (x).begin(), (x).end()
 #define MOD 1e9+7
+#define str string
 
 int main(){
     ios::sync_with_stdio(false);
