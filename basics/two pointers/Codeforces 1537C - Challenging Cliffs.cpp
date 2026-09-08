c46cfc29fd7566dd087ac4694270079f67493c23
Derff <srderfflinqer@gmail.com>
2026-09-08 01:03:38 -0500

    two pointers

---
+++ basics/two pointers/Codeforces 1537C - Challenging Cliffs.cpp
@@ -0,0 +1,122 @@
+/*
+ ⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀  ⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀  ⢀⢴⣶⣆ 
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ 
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ 
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀ ⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀ ⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉ 
+
+        Vuelve emi
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define sz(x) (ll)(x).size()
+#define rep(i, a, b) for (ll i=(a); i<(b); i++)
+#define vin vector<int>
+#define vll vector<ll>
+#define vch vector<char>
+#define vpii vector<pair<int,int>>
+#define vpll vector<pair<ll,ll>>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define all(x) (x).begin(), (x).end()
+#define MOD 1e9+7
+#define str string
+
+void solve(){
+    ll n;
+    cin>>n;
+
+    vll v(n);
+    rep(i,0,n) cin>>v[i];
+
+    sort(v.begin(), v.end());
+
+    if(n==2){
+        rep(i,0,n) cout<<v[i]<<" ";
+        cout<<"\n";
+        return;
+    }
+
+    ll min_s=LLONG_MAX;
+    ll ei, ej;
+    rep(i,1,n){
+        ll diff=v[i]-v[i-1];
+        if(diff<min_s){
+            min_s=diff;
+            ei=i-1;
+            ej=i;
+        }
+        if(min_s==0) break;
+    }
+
+    vll ans(n);
+    ans[0]=v[ei];
+    ans[n-1]=v[ej];
+
+    v[ei]=-1;
+    v[ej]=-1;
+
+    vll nms(n-2);
+    ll ind=0;
+    ll i=0;
+
+    while(i<n-2){
+        if(v[ind]!=-1) nms[i++]=v[ind++];
+        else ind++;
+    }
+
+    ll mins=0;
+    ll maxs=n-2;
+
+    rep(i,0,n-2){
+        if(nms[i]>ans[n-1]){
+            maxs=i;
+            break;
+        }
+    }
+
+    ll ind1=1;
+    ll ind2=0;
+    ll ind_ceil=maxs;
+
+    while(maxs<n-2){
+        ans[ind1++]=nms[maxs++];
+    }
+    while(ind2<ind_ceil){
+        ans[ind1++]=nms[ind2++];
+    }
+
+    rep(i,0,n){
+        cout<<ans[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ll t;
+    cin>>t;
+
+    while(t--){
+        solve();
+    }
+
+    return 0;
+}
